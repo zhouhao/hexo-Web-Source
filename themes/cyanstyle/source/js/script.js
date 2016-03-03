2df06c1e57074906580678f474d1177d4c596c66
Hao Zhou <codeashobby@gmail.com>
2016-03-02 21:42:07 -0500

    update

--- themes/cyanstyle/source/js/script.js
+++ themes/cyanstyle/source/js/script.js
@@ -31,60 +31,6 @@
     stopSearchAnim();
   });
 
-  // Share
-  /*$('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
-
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      offset = $this.offset();
-
-    if ($('#' + id).length){
-      var box = $('#' + id);
-
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
-      }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-			'<a href="http://tieba.baidu.com/f/commit/share/openShareApi?url=' + encodedUrl + '" class="article-share-tieba" target="_blank" title="百度贴吧"></a>',
-			'<a href="http://service.weibo.com/share/share.php?url=' + encodedUrl + '" class="article-share-weibo" target="_blank" title="新浪微博"></a>',
-			'<a href="http://share.v.t.qq.com/index.php?c=share&a=index&url=' + encodedUrl + '" class="article-share-tqq" target="_blank" title="腾讯微博"></a>',
-			'<a href="http://widget.renren.com/dialog/share?resourceUrl=' + encodedUrl + '" class="article-share-renren" target="_blank" title="人人"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
-
-      var box = $(html);
-
-      $('body').append(box);
-    }
-
-    $('.article-share-box.on').hide();
-
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });*/
-
   // Caption
   $('.article-entry').each(function(i){
     $(this).find('img').each(function(){
@@ -134,7 +80,7 @@
 
     $container.removeClass('mobile-nav-on');
   });
-  
+
     // link
     var $linkUl = $('#link-list');
     var $list = $('#link-list li');
@@ -146,24 +92,4 @@
         $linkUl.append($list.eq(ran));
         $list.splice(ran, 1);
     }
-    
-    // subtitle
-    var subtitleList = [
-        "“刚才你问我啊，我可以回答你一句‘无可奉告’。”——长者",
-        "“你们媒体千万要记着，不要‘见得风，是得雨’。”——长者",
-        "“连任也要按照香港的法律啊。”——长者",
-        "“所以说媒体呀还是要提高自己的知识水平，识得唔识得啊？”——长者",
-        "“西方的哪一个国家我没去过？”——长者",
-        "- “你觉得董先生连任好不好啊？” - “吼啊！”——长者",
-        "“你们啊，不要想喜欢弄个大新闻，说现在已经钦定了，就把我批判一番。”——长者",
-        "“你们有一个好，全世界跑到什么地方，你们比其他的西方记者跑得还快。”——长者",
-        "“你问我资瓷不资瓷，我是资瓷的。”——长者",
-        "“你们毕竟还图样.明白我的意思吧。”——长者",
-        "“I am angry！你们这样子是不行的。”——长者",
-        "“美国的华莱士，比你们不知要高到哪里去了，我跟他谈笑风生。”——长者",
-        "“问来问的问题呀，都 too young，too simple，sometimes naive。”——长者",
-        "“我今天是作为一个长者跟你们讲。我就讲中国有一句话叫‘闷声大发财’。”——长者"
-    ];
-    var i = Math.floor(Math.random() * subtitleList.length);
-    $('#subtitle').text(subtitleList[i]);
 })(jQuery);
